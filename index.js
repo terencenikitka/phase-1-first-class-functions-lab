8d5ff9f015cbd4933b0ee0ff6167fa54b7e43b22
Nikita <terencekity@gmail.com>
2023-09-12 10:59:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = arr=> arr.slice(0,2)
+const returnLastTwoDrivers = arr=> arr.slice(arr.length-2,arr.length)
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier (arg){
+    return function (price){
+        return price*arg
+    }
+}
+const fareDoubler = par => createFareMultiplier(par)(2)
+const fareTripler = par => createFareMultiplier(par)(3)
+function selectDifferentDrivers(arr,cb){
+    return cb(arr)
+}
